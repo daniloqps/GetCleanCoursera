0f657796e8fa1d05814312fef836a814681b40db
Danilo Pinheiro <daniloqps@users.noreply.github.com>
2015-10-20 14:59:40 -0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,3 +5,30 @@
 #Step 3: Uses descriptive activity names to name the activities in the data set
 #Step 4: Appropriately labels the data set with descriptive variable names. 
 #Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+CODE HERE!
+
+#Rules
+
+#Please upload the tidy data set created in step 5 of the instructions. Please upload your data set 
+#as a txt file created with write.table() using row.name=FALSE (do not cut and paste a dataset directly 
+#into the text box, as this may cause errors saving your submission).
+
+      #Has the student submitted a tidy data set? Either a wide or a long form of the data is acceptable if 
+      #it meets the tidy data principles of week 1 (Each variable you measure should be in one column, 
+      #Each different observation of that variable should be in a different row).
+      
+      
+#Please submit a link to a Github repo with the code for performing your analysis. 
+#The code should have a file run_analysis.R in the main directory that can be run as 
+#long as the Samsung data is in your working directory. The output should be the tidy 
+#data set you submitted for part 1. You should include a README.md in the repo describing 
+#how the script works and the code book describing the variables.
+
+      #Did the student submit a Github repo with the required scripts?
+      
+      #Was code book submitted to GitHub that modifies and updates the 
+      #codebooks available to you with the data to indicate all the variables and 
+      #summaries you calculated, along with units, and any other relevant information?
+
+      #I was able to follow the README in the directory that explained what the analysis files did. 

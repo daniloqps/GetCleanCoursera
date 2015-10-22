bb95a62f8ccdd7ec7fcef329242e8892c71cc6a3
Danilo Pinheiro <daniloqps@users.noreply.github.com>
2015-10-22 03:06:04 -0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,53 @@
 #Step 4: Appropriately labels the data set with descriptive variable names. 
 #Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-CODE HERE!
+options("scipen"=-100, "digits" = 7)
+options("scipen"=100, "digits" = 7)
+library(data.table)
+
+# FILES CONSTANTS
+dir                     <- "UCI HAR Dataset"
+dir_test                <- "test"
+dir_train               <- "train"
+file_activity_labels    <- paste(dir,            "activity_labels.txt", sep = "\\")
+file_features           <- paste(dir,            "features.txt",        sep = "\\")
+file_X_train            <- paste(dir, dir_train, "X_train.txt",         sep = "\\")
+file_y_train            <- paste(dir, dir_train, "y_train.txt",         sep = "\\")
+file_subject_train      <- paste(dir, dir_train, "subject_train.txt",   sep = "\\")
+file_X_test             <- paste(dir, dir_test,  "X_test.txt",          sep = "\\")
+file_y_test             <- paste(dir, dir_test,  "y_test.txt",          sep = "\\")
+file_subject_test       <- paste(dir, dir_test,  "subject_test.txt",    sep = "\\")
+
+ActLabels <- fread(file_activity_labels, col.names = c("ID", "DESC"), header = FALSE,  stringsAsFactors = FALSE)
+Features  <- t(fread(file_features,  drop = 1, col.names = c("DESC"), header = FALSE,  stringsAsFactors = FALSE))
+
+DTrain    <- fread(file_X_train, col.names = Features, header = FALSE, stringsAsFactors = FALSE, nrows=10 ) #ALERT - 10 only test
+DTest     <- fread(file_X_test,  col.names = Features, header = FALSE, stringsAsFactors = FALSE, nrows=10 ) #ALERT - 10 only test
+
+LTrain    <- fread(file_y_train, col.names = c("LABEL"), header = FALSE, stringsAsFactors = FALSE, nrows=10 ) #ALERT - 10 only test
+LTest     <- fread(file_y_test,  col.names = c("LABEL"), header = FALSE, stringsAsFactors = FALSE, nrows=10 ) #ALERT - 10 only test
+
+STrain    <- fread(file_subject_train, col.names = c("SUBJECT"), header = FALSE, stringsAsFactors = FALSE, nrows=10 ) #ALERT - 10 only test
+STest     <- fread(file_subject_test , col.names = c("SUBJECT"), header = FALSE, stringsAsFactors = FALSE, nrows=10 ) #ALERT - 10 only test
+
+FullTrain <- cbind(LTrain, STrain, DTrain)
+FullTest  <- cbind(LTest,  STest,  DTest)
+
+CompleteSet <- rbind(FullTrain, FullTest)
+
+
+
+
+x<- c(1:31)
+
+class(x)
+DTrain <- DTrain[FALSE,]
+class(DTrain)
+DTest[[10,561]]
+
+str(LTest)
+
+
 
 #Rules
 
